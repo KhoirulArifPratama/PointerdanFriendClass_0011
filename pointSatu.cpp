3a410d57087ad316a07fb28510d8eeafca40a2ce
KhoirulArifPratama <khoirularif932@gmail.com>
2025-05-05 10:53:25 +0700

    full commit

---
+++ pointSatu.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    int nim;
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+    mahasiswa mhs{1}; // object mhs
+    mhs.showNim();    // Member Acces Operator
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2; // Member Acces Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs->nim = 3; // Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
