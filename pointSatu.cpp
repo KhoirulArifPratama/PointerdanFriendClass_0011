821480706e0b027e3f0a738e54db7db30034c943
KhoirulArifPratama <khoirularif932@gmail.com>
2025-05-05 10:56:32 +0700

    pointerdereference

--- pointSatu.cpp
+++ pointSatu.cpp
@@ -10,3 +10,18 @@ public:
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa mhs{1}; // object mhs
+    mhs.showNim();    // Member Acces Operator
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2; // Member Acces Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs->nim = 3; // Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
